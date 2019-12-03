ef2300678bf5de34e1ab8d09945e0f8c52a0fb13
diegoldiazr <diegoldiazr@gmail.com>
2019-12-03 16:14:34 +0100

    creacion de aplicaciones

---
+++ prices2/gestionListaCompra/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestionlistacompraConfig(AppConfig):
+    name = 'gestionListaCompra'
---
+++ prices2/gestionTiendas/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class GestiontiendasConfig(AppConfig):
+    name = 'gestionTiendas'
